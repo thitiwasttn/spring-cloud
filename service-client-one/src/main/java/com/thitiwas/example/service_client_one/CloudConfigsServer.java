6d9bb85dcb7f21737a9ff14c8de6c87ea80f6595
Thitiwas Nupan <55071973+thitiwasttn@users.noreply.github.com>
2025-05-05 21:27:22 +0700

    up

---
+++ service-client-one/src/main/java/com/thitiwas/example/service_client_one/CloudConfigsServer.java
@@ -0,0 +1,15 @@
+package com.thitiwas.example.service_client_one;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@RefreshScope
+@Configuration
+public class CloudConfigsServer {
+    @Value("${my-config.name: default value}")
+    private String name;
+
+}
--- service-client-one/src/main/java/com/thitiwas/example/service_client_one/controller/HomeController.java
+++ service-client-one/src/main/java/com/thitiwas/example/service_client_one/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.thitiwas.example.service_client_one.controller;
 
+import com.thitiwas.example.service_client_one.CloudConfigsServer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/service-one")
 public class HomeController {
 
+    private final CloudConfigsServer cloudConfigsServer;
+
+    public HomeController(CloudConfigsServer cloudConfigsServer) {
+        this.cloudConfigsServer = cloudConfigsServer;
+    }
+
     @GetMapping("/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Hello from service-one!");
+    }
+
+
+    @GetMapping("/name")
+    public ResponseEntity<String> name() {
+        return ResponseEntity.ok("the following value read form config server = " + cloudConfigsServer.getName());
     }
 }
